30af52b73f8613cb6dfe8302ffa17cfc2edcda78
Madhavireddy0908 <112148656+Madhavireddy0908@users.noreply.github.com>
2024-10-17 19:52:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let express = require("express");
 require("./dbconfig/dbcon");
 let cors = require("cors");
-let studentRouting = require("./Router/studentRouter");
-const empRouting = require("./Router/empRouter");
 const signupRouting = require("./Router/signupRouter");
 const treatmentRouting = require("./Router/treatmentRouter");
 const offerRouting = require("./Router/offerRouter");
@@ -13,8 +11,6 @@ const doctorRouting = require("./Router/doctorRouter");
 let app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/student", studentRouting);
-app.use("/employee", empRouting);
 app.use("/signup", signupRouting);
 app.use("/treatment", treatmentRouting);
 app.use("/offer", offerRouting);
