165d14416f64187238f2dee8f521aa24da0ab3e9
Madhavireddy0908 <112148656+Madhavireddy0908@users.noreply.github.com>
2024-10-17 20:19:48 +0530

    Update dbcon.js

--- dbconfig/dbcon.js
+++ dbconfig/dbcon.js
@@ -1,3 +1,2 @@
 let mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/dbelearn");
-//mongoose.connect("mongodb+srv://madhavireddy0908:833JZGFORARQiCXl@dbsept.ffgem.mongodb.net/?retryWrites=true&w=majority&appName=dbsept");
